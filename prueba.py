1839e4310d3e5ed31e7a09f310ec44adc6e8b573
DaytonGarcia <dayton.garcia@gmail.com>
2016-02-05 20:09:24 -0600

    agregando gluon

--- prueba.py
+++ prueba.py
@@ -24,10 +24,16 @@ client.post('user/login', data=data)
 client.get('user/profile')
 assert 'Welcome danigay' in client.text
 
+# Prueba funcional para la funcion Agregar Especialidad
+# Detalle='especialidad' Detalle es el atributo como lo defini en dbwizard, si hay mas atributos se agregan con "," 
+# _formname='registro' Esto se queda asi
+# data1 es el nombre del objeto que tiene los mismo atributos que mi tabla
+# client.post('especializacion', data=data1) especializacion es el nombre de la pagina html, que va despues de default/ 
 data1 = dict(Detalle='especialidad',
             _formname='registro')
 client.post('especializacion', data=data1)
-
+#######Aca termina la funcionalidad, debe haber insertado especialidad en mi base de datos#######################
+#######Web2py debe estar corriendo
 
 # logout
 client.get('user/logout')
@@ -36,4 +42,4 @@ client.get('user/logout')
 print
 for method, url, status, t in client.history:
     if  status==200:
-        print url+' superada'
+        print url+' superada' + t
